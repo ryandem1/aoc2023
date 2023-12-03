a76a54d83b4eda06ffd9bef37f9afd435eaeda1c
ryandemarigny <ryandemarigny@gmail.com>
2023-12-02 21:13:26 -0800

    Done day 2 part 1

--- solutions/day2.go
+++ solutions/day2.go
@@ -3,8 +3,112 @@
 
 package solutions
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"unicode"
+)
+
 func Day2Part1(inputPath string) (string, error) {
-	return "", nil
+	cubeCapacityByColor := map[string]int{
+		"red":   12,
+		"green": 13,
+		"blue":  14,
+	}
+	sumPossibleGameIDs := 0
+
+	file, err := os.Open(inputPath)
+	if err != nil {
+		return "", err
+	}
+	defer func(file *os.File) {
+		_ = file.Close()
+	}(file)
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		colonSeen := false
+		gamePossible := true
+		var gameID strings.Builder
+		var quantity strings.Builder
+		var color strings.Builder
+
+		for _, char := range line {
+
+			if char == ':' {
+				colonSeen = true
+			}
+
+			if unicode.IsDigit(char) && !colonSeen {
+				gameID.WriteRune(char)
+			}
+
+			if unicode.IsDigit(char) && colonSeen {
+				quantity.WriteRune(char)
+			}
+
+			if unicode.IsLetter(char) && colonSeen {
+				color.WriteRune(char)
+			}
+
+			if char == ',' || char == ';' {
+				quantityValue, err := strconv.Atoi(quantity.String())
+				if err != nil {
+					return "", err
+				}
+
+				capacity, ok := cubeCapacityByColor[color.String()]
+				if !ok {
+					return "", fmt.Errorf("invalid color: %s", color.String())
+				}
+
+				if quantityValue > capacity {
+					gamePossible = false
+				}
+				quantity.Reset()
+				color.Reset()
+			}
+		}
+
+		quantityValue, err := strconv.Atoi(quantity.String())
+		if err != nil {
+			return "", err
+		}
+
+		capacity, ok := cubeCapacityByColor[color.String()]
+		if !ok {
+			return "", fmt.Errorf("invalid color: %s", color.String())
+		}
+
+		if quantityValue > capacity {
+			gamePossible = false
+		}
+		quantity.Reset()
+		color.Reset()
+
+		gameIDValue, err := strconv.Atoi(gameID.String())
+		if err != nil {
+			return "", err
+		}
+
+		if gamePossible {
+			sumPossibleGameIDs += gameIDValue
+		}
+
+		gameID.Reset()
+	}
+
+	if err = scanner.Err(); err != nil {
+		return "", nil
+	}
+
+	return strconv.Itoa(sumPossibleGameIDs), nil
 }
 
 func Day2Part2(inputPath string) (string, error) {
