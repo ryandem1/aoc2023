97ebfab83d9447ad72992d5952f459c389b6dfbe
ryandemarigny <ryandemarigny@gmail.com>
2023-12-03 21:27:23 -0800

    Added documentation

--- solutions/day4.go
+++ solutions/day4.go
@@ -1,6 +1,11 @@
+// Day 4: Scratchcards
+// https://adventofcode.com/2023/day/4
+
 package solutions
 
 func Day4Part1(inputPath string) (string, error) {
+	// Take a seat in the large pile of colorful cards.
+	// How many points are they worth in total?
 	return "", nil
 }
 
