f960507987d1e87183bb13effd1d96fdd55b5945
ryandemarigny <ryandemarigny@gmail.com>
2023-12-10 17:28:04 -0800

    Day 6 Part 1 input parsing

--- solutions/day6.go
+++ solutions/day6.go
@@ -3,9 +3,52 @@
 
 package solutions
 
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
 func Day6Part1(inputPath string) (string, error) {
 	// Determine the number of ways you could beat the record in each race.
 	// What do you get if you multiply these numbers together?
+
+	file, err := os.Open(inputPath)
+	if err != nil {
+		return "", err
+	}
+	var times []int
+	var distances []int
+
+	scanner := bufio.NewScanner(file)
+
+	lineNum := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		lineSplit := strings.Split(line, " ")
+		for _, item := range lineSplit {
+			cleanedItem := strings.Trim(item, " ")
+			itemVal, err := strconv.Atoi(cleanedItem)
+			if err != nil {
+				continue
+			}
+			if lineNum == 0 {
+				times = append(times, itemVal)
+			} else if lineNum == 1 {
+				distances = append(distances, itemVal)
+			} else {
+				continue
+			}
+		}
+
+		lineNum++
+	}
+
+	err = scanner.Err()
+	if err != nil {
+		return "", err
+	}
 	return "", nil
 }
 
