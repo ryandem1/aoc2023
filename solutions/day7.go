ac037951d863b6be94a426fac9679002674ee2a2
ryandemarigny <ryandemarigny@gmail.com>
2023-12-10 20:39:02 -0800

    Added some docs

--- solutions/day7.go
+++ solutions/day7.go
@@ -1,6 +1,11 @@
+// Day 7: Camel Cards
+// https://adventofcode.com/2023/day/7
+
 package solutions
 
 func Day7Part1(inputPath string) (string, error) {
+	// Find the rank of every hand in your set.
+	// What are the total winnings?
 	return "", nil
 }
 
